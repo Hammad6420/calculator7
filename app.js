93a1059733502651a58ebfd4a198f6e0eb31aa88
MUHAMMAD HAMMAD <hammadriaz28@gmail.com>
2020-06-28 19:01:11 +0500

    UI

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
 var i = 0; ii = 0;
-var num = []; number = 0; lastResult = 0; clearForNew = 0; delNew = 0; yy = 0; syms = [];num3=0;multiplyPre=0;
+var num = []; number = 0; lastResult = 0; clearForNew = 0; delNew = 0;  dot=0;yy=0;
 
 
 // Numbers
-
 function getNo(no) {
     // dont show result
     document.getElementById('resultHead').innerText = '';
     // clear output
     if (clearForNew == 0) {
         document.getElementById('inpOut').innerText = '';
+        dot=0;
     }
     // delete no
     if (delNew == 1) {
@@ -21,16 +21,28 @@ function getNo(no) {
     }
     // use old not delete
     if (delNew == 0) {
+        if (no=='.') {
+            ++dot;
+            if (dot==1) {
+                clearForNew = clearForNew + 1;
+                document.getElementById('inpOut').innerText += no;
+                x = document.getElementById('inpOut').innerText;
+            }
+        }else{
         clearForNew = clearForNew + 1;
         document.getElementById('inpOut').innerText += no;
         x = document.getElementById('inpOut').innerText;
-    }
+        }
+}        
     // combine word
     if ((x[x.length - 1]) != '+' && (x[x.length - 1]) != '-' && (x[x.length - 1]) != '/' && (x[x.length - 1]) != '=' && (x[x.length - 1]) != 'X') {
-        num[i] = no;
+        num = x;
         i = i + 1;
-        return number = parseInt(num.join(''));
+        number=num;
     }
+    stp=0;
+    numOper='';
+    numOper=number;
 }
 
 
@@ -38,110 +50,94 @@ function getNo(no) {
 // symbols
 function getsym(sym) {
     // check for double symbol
-    if ((x[x.length - 1]) != '+' && (x[x.length - 1]) != '-' && (x[x.length - 1]) != '/' && (x[x.length - 1]) != '=' && (x[x.length - 1]) != 'X') {
+    xx=document.getElementById('preceding').innerText.split('');
+    if (number!='undefined') {
+    if ((xx[xx.length - 1]) != '+' && (xx[xx.length - 1]) != '-' && (xx[xx.length - 1]) != '/' && (xx[xx.length - 1]) != '=' && (xx[xx.length - 1]) != 'X' ) {
         if (delNew == 0) {
             document.getElementById('inpOut').innerText = '';
-            document.getElementById('preceding').innerText += number + sym;
+            document.getElementById('preceding').innerText +=number+sym;
+        }
+        }else{
+            document.getElementById('preceding').innerText += document.getElementById('inpOut').innerText;
         }
         // new no
         num = [];
         i = 0;
+        dot=0;
         clearForNew = 0;
+
         // delete no
         if (delNew == 1) {
             number = y;
-            document.getElementById('preceding').innerText += number + sym;
+            document.getElementById('preceding').innerText += sym;
             delNew = 0;
         }
-
-        // symbole get for calculation
-        syms[yy] = sym;
-
-        // multiple set
-                if (syms[0]==syms[1]) {
-               multiplyPre=1;
-                }
-
-        if (yy == 1) {
-            sym = syms[0];
-            syms[0] = syms[1]
-            yy = 0;
-        }
-        yy = 1;
-        // set num1 and num2 for operations
-        if (ii == 0 || ii > 1) {
-            if (syms[1] != 'X' && syms[1] != '/') { 
-                num1 = number;
-            }else{
-                num1=number;
-        }
     }
-        if (ii == 1) {
-            if (syms[1] != 'X' && syms[1] != '/') {
-                ii = ii + 1;
-                num2 = number;
-            } else {
-                num2 = number;
-            }
-        } else {
-            ii = ii + 1;
-            if (syms[1] != 'X' && syms[1] != '/') {
-                num2 = lastResult;
-            } else {
-                num2=number;
-            }
-            console.log(num1, num2,num3,lastResult)
-        }
-
-        if (ii >= 0) {
-            if (sym == 'X') {
-                multiply()
-            }
-            if (sym == '+') { add() }
-            if (sym == '-') { substract() }
-            if (sym == '/') { divide() }
-        }
-    }
-    if (multiplyPre==1) {
-        console.log(num1,num2)
-        if (syms[1]=='X') {
-            multiply()
-        }
-        if (syms[1]=='/') {
-            divide()
-        }
-    }
+number='';
+numOper='';
 }
+
+
 
 // work
-
-function add() {
-    lastResult = parseInt(num1) + parseInt(num2);
-    document.getElementById('inpOut').innerText = '';
-    document.getElementById('inpOut').innerText = lastResult;
-    document.getElementById('resultHead').innerText = 'RESULT'
+function percentage() {
+    number = document.getElementById('inpOut').innerText = lastResult * (100 / numOper) + '%';
+    lastResult=number;
 }
-function substract() {
-    lastResult = parseInt(num1) - parseInt(num2);
-    document.getElementById('inpOut').innerText = '';
-    document.getElementById('inpOut').innerText = lastResult;
-    document.getElementById('resultHead').innerText = 'RESULT'
+function squareRoot() {
+    number = document.getElementById('inpOut').innerText = numOper ** (1 / 2);
+    numOper=number;
+    lastResult=number;
 }
-function multiply() {
-    console.log(lastResult)
-    lastResult = (parseInt(num2) * parseInt(num1));
-    console.log(lastResult)
-    document.getElementById('inpOut').innerText = '';
-    document.getElementById('inpOut').innerText = lastResult;
-    document.getElementById('resultHead').innerText = 'RESULT'
+function square() {
+    number = document.getElementById('inpOut').innerText = numOper ** 2;
+    numOper=number;    lastResult=number;
 }
-function divide() {
-    lastResult =  (parseInt(num1) / parseInt(num2));
-    document.getElementById('inpOut').innerText = '';
-    document.getElementById('inpOut').innerText = lastResult;
-    document.getElementById('resultHead').innerText = 'RESULT'
+function perNo() {
+    number = document.getElementById('inpOut').innerText = 1 / numOper;
+    numOper=number;    lastResult=number;
 }
-
+var neg = 0;
+function multipleMinus() {
+    if (neg == 1) {
+        number = document.getElementById('inpOut').innerText = number * 1;
+        neg = 0;
+        numOper=number;    lastResult=number;
+    }
+    if (neg == 0) {
+        number = document.getElementById('inpOut').innerText = numOper * -1;
+        neg = neg + 1;
+        numOper=number;    lastResult=number;
+    }
+}
+// solve
+var eq = 0;
+var stp=0;
+function equalTo() {
+    clearForNew=0;
+    stp=stp+1;
+    xx=document.getElementById('preceding').innerText.split('');
+    if (stp==1 || (xx.endsWith('+') && xx.endsWith('-') && xx.endsWith('X') && xx.endsWith('/') )) {
+     
+    eq = document.getElementById('preceding').innerText;
+    eqNo = document.getElementById('inpOut').innerText;
+            eq = document.getElementById('preceding').innerText += eqNo;
+            eqOld = eq;
+            eq = eq.split('')
+            for (let index = 0; index < eq.length; index++) {
+                if (eq[index] == 'X') {
+                    eq[index] = '*';
+                }
+            }
+            eq = eq.join('')
+            console.log(eq)
+            lastResult = document.getElementById('inpOut').innerText = eval(eq);
+            document.getElementById('preceding').innerText = eqOld;
+            document.getElementById('resultHead').innerText = 'RESULT';  
+            number='';
+    }
+    numOper=lastResult;
+    }
 
 // DELETE
 function del() {
@@ -159,21 +155,17 @@ function del() {
 function clearCE() {
     document.getElementById('preceding').innerText = "";
     document.getElementById('inpOut').innerText = '0';
-    num1 = 0; num2 = 0; lastResult = 0; number = 0; clearForNew = 0;
+    num1 = 0; num2 = 0; lastResult = 0; number = 0; clearForNew = 0;numOper=0;
 }
+// correction preceding
 function clearC() {
-    document.getElementById('preceding').innerText = "";
-    document.getElementById('inpOut').innerText = '0';
-    num1 = 0; num2 = 0; lastResult = 0; number = 0; clearForNew = 0;
+    var c = document.getElementById('preceding').innerText.split('');
+    console.log(x)
+    c.pop();
+    yy = c.join('');
+    console.log(yy)
+    document.getElementById('preceding').innerText = yy;
 }
-
-
-
-
-
-
-
-
 
 
 
